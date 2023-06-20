38deaf030e7d360d717fd7405fd7fd42bbf68377
Abdul Rauof Nasery <rauf.nasery143@gmail.com>
2023-06-20 13:11:43 -0600

    account page

---
+++ Capstones.client/src/components/MyTopicsCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="row">
+        <div class="col-md-4 col-10 py-2">
+            <div class="card">
+                <h3>Subject: {{ topicsProp?.subject }}</h3>
+                <h3>Level: {{ topicsProp?.level }}</h3>
+            </div>
+        </div>
+    </section>
+</template>
+
+
+<script>
+import { Topic } from '../models/Topic.js';
+
+export default {
+
+    props: {
+        topicsProp: { type: Topic, required: true }
+    },
+    setup() {
+        return {
+
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
