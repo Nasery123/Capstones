1427c21ac47c5cda59752843b03f59a2bd9a7ce1
ScottWallin <scott.wallin@me.com>
2023-06-21 11:17:55 -0600

    AppStateCHANGES

--- Capstones.client/src/AppState.js
+++ Capstones.client/src/AppState.js
@@ -5,8 +5,22 @@ export const AppState = reactive({
   user: {},
   /** @type {import('./models/Account.js').Account} */
   account: {},
-
   topics: [],
-  myTopics: []
+  myTopics: [],
+   /** @type {import('./models/Room.js').Room} */
+  room: [],
+  /** @type {import('./models/Room.js').Room} */
+  rooms: [],
+  /** @type {import('./models/Message.js').Message[]} */
+  messages: [],
+  /** @type {import('./models/NewUser.js').NewUser[]} */
+  users: [],
 
+  /** @type {import('./models/Channel.js').Channel||null} */
+  channels: null,
+
+  /** @type {import('./models/Channel.js').Channel||null} */
+  channel: null,
+  /** @type {import('./models/Channel.js').Channel||null} */
+  editChannel: null,
 })
---
+++ Capstones.client/src/services/MessagesService.js
@@ -0,0 +1,11 @@
+import { Message } from "../models/Message.js";
+import { logger } from "../utils/Logger.js";
+import { api } from "./AxiosService.js"
+
+class MessagesService {
+async createMessage(messageData) {
+  const res = await api.post("api/messages", messageData);
+  logger.log(new Message(res.data))
+} 
+}
+export const messagesService = new MessagesService()
